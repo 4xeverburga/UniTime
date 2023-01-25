c2afd210ae10bf57c23fdc940e9bd5e94fce5668
Italo <italo.casanova.d@uni.pe>
2023-01-25 12:59:11 -0500

    minnor changes

---
+++ src/components/Home/AliadosComp.vue
@@ -0,0 +1,29 @@
+<template>
+<v-card>
+    <v-card-title>
+        <h2>Aliados</h2>
+    </v-card-title>
+    <v-list>
+    <v-list-item
+        v-for="(name, i) in aliados" :key="i" link>
+        <v-list-content>
+            <v-list-content-tittle>{{ name }}</v-list-content-tittle>
+        </v-list-content>
+
+    </v-list-item>
+</v-list>
+</v-card>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const aliados = ref([
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ])
+
+</script>
