c3d3e8f7ccbbcedb5e8c46632493cbd6fc100163
Italo <italo.casanova.d@uni.pe>
2023-01-29 11:01:06 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-set -e 
+set -e
 
-npm run build 
-cd dist 
+npm run build
+cd dist
 
 # gh pages hosting workaround
 cp index.html 404.html
@@ -11,6 +11,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com: italo-casanova/ccat_rework.git master:gh-pages    
+git push -f git@github.com:italo-casanova/ccat_rework.git master:gh-pages
 
 cd -
