cce2c810d5872a4a9bc440067b0a35135965c3cb
4verBurga <ever.burga.p@uni.pe>
2023-02-04 23:11:55 -0500

    fixed router

--- src/views/public/projects/ProjectView.vue
+++ src/views/public/projects/ProjectView.vue
@@ -1,10 +1,32 @@
+<!-- project portfolio -->
+
 <template>
 <!-- title -->
+<h1>
+    <span class="text-primary">Portafolio de proyectos</span>
+</h1>
+<!-- search bar -->
+<span> search bar</span>
 
-<!-- seach bar -->
-
+<!-- pagination component -->
 
 </template>
+
+<script setup>
+
+const items = [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 }
+]
+
+</script>
 
 <style scoped>
 
