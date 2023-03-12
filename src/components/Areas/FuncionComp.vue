777f34bdfe8cb89b07431ff60919ea6faa4be0e1
Italo <italo.casanova.d@uni.pe>
2023-03-12 01:12:50 -0500

    [UPDATE]: area view

--- src/components/Areas/FuncionComp.vue
+++ src/components/Areas/FuncionComp.vue
@@ -1,32 +1,53 @@
 <template>
-	<v-container >
-		<v-row >
+		<v-row
+			no-gutters
+			no-line
+			>
 			<v-col
-				cols="12"
-				sm="6"
-				md="8"
+				no-gutters
+				cols="6"
 			>
 				<v-card
-				>
+					>
 				<v-card-title>  Funcion del area</v-card-title>
 				<v-card-text>  flor de la Funcion del area</v-card-text>
 				</v-card>
 			</v-col>
 			<v-col
+				no-gutters
 				cols="6"
-				md="4"
 			>
 				<v-card
+				class="pa-4 text-center secondary text-no-wrap rounded-lg"
+				color="#778da94d"
 				>
-				gerente y sub
+				<v-container>
+
+					<v-row>
+						<v-col
+							no-gutters
+							cols="6"
+						>
+						director
+						</v-col>
+
+						<v-col
+							no-gutters
+							cols="6"
+						>
+						chacal
+						</v-col>
+
+					</v-row>
+				</v-container>
+
 				</v-card>
 			</v-col>
 		</v-row>
-	</v-container>
 </template>
 
 <style scoped>
-
-
-
 </style>
+
+<script setup>
+</script>
