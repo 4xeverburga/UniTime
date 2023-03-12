ef4a8e55405c4a866f34e561453b885864c33ffd
Oscar Torres <oscar.torres.m@uni.pe>
2023-03-12 00:10:36 -0500

    imagen

--- src/components/ProjectDetail/ProjectDescription.vue
+++ src/components/ProjectDetail/ProjectDescription.vue
@@ -1,44 +1,50 @@
 <template>
-	<v-card
-		elevation="0"
-		class="mx-13"
+<v-card
+	elevation="0"
+	class="mx-11"
+>
+	<v-card-title
+		class="my-13"
+		width="80%"
 	>
-		<v-card-title
-		class="mt-13git "
-	>
-		<p class="text-h2">
+		<p 
+		class="text-lg-h2 text-md-h4 text-h5 text-center">
 			<span class="text-decoration-underline" style="color:#1976D2">Portafolio de Proyectos</span>
 			<span class="font-weight-medium">/Nombre del proyecto</span>
 		</p>
+
 	</v-card-title>
-
-
-	<v-card
-		class="text-start"
-		elevation="0"
+	<v-card-text
+		class="d-flex justify-center mx-13"
 	>
-		<v-card-title class="mb-10">
-			<p class="text-h2 font-weight-medium">Nombre del proyecto</p>
-		</v-card-title>
+		<v-card
+			elevation="0"
+			width="45%"
+		>
+			<v-card-title class="mb-10">
+				<p class="text-h2 font-weight-medium">Nombre del proyecto</p>
+			</v-card-title>
 
-		<v-card-text>
-		El Centro Cultural Avanzada Tecnológica (CCAT) es una organización de estudiantes y egresados fundada en la
-		Facultad de Ingeniería Industrial y de Sistemas de la Universidad Nacional de Ingeniería, el 22 de mayo de 1991,
-		como una iniciativa estudiantil cuya motivación especial es emprender el reto de complementar la formación empresarial,
-		académica, cultural y humana de los futuros profesionales para lograr con ellos la construcción de una mejor sociedad.
-		</v-card-text>
-	</v-card>
+			<v-card-text>
+				El Centro Cultural Avanzada Tecnológica (CCAT) es una organización de estudiantes y egresados fundada en la
+				Facultad de Ingeniería Industrial y de Sistemas de la Universidad Nacional de Ingeniería, el 22 de mayo de 1991,
+				como una iniciativa estudiantil cuya motivación especial es emprender el reto de complementar la formación empresarial,
+				académica, cultural y humana de los futuros profesionales para lograr con ellos la construcción de una mejor sociedad.
+			</v-card-text>
+		</v-card>
+		<div class="ma-4">
+		<v-img
+			class="bg-white"
+			width="300"
+			:aspect-ratio="1"
+			:src="PatataImage"
+			cover
+		></v-img>
+    </div>
 
-	<v-img
-		id="conocenos-logo"
-		lazy-src="@/assets/patata.jpg"
-		height="240"
-		width="240"
-		:src= "PatataImage"
-		contain
-	/>
-	</v-card>
-	
+	</v-card-text>
+</v-card>
+
 
 </template>
 
