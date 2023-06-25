8500183aa5ad36a2f86cc97b69b2cba75248a85a
GabrielF2311 <82108023+GabrielF2311@users.noreply.github.com>
2023-06-25 18:08:45 -0500

    Update MembersView.vue

--- src/views/public/grupos/MembersView.vue
+++ src/views/public/grupos/MembersView.vue
@@ -3,22 +3,90 @@
     <v-card-title class="d-flex align-center">
       <v-icon class="mr-2">mdi-account-group</v-icon>
       <h2 class="group-name">Trabajo de DBD</h2>
+      <v-spacer></v-spacer>
+      <div class="user-profile">
+        <v-icon>mdi-account-circle</v-icon>
+        <span class="username">Nombre de usuario</span>
+        <v-btn icon color="primary" @click="logout">
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
+      </div>
     </v-card-title>
-  
-        <v-card-title>
-          <v-icon class="mr-2">mdi-format-quote-open</v-icon>
-          <h4>Descripción del grupo:</h4>
-        </v-card-title>
-  
-        <v-card-text>
-          Grupo formado por estudiantes que están llevando el curso de Diseño de Base de Datos, para la elaboración de un prototipo en base a la gestión de horarios.
-        </v-card-text>
-      </v-card>
 
-  </template>-
+    <v-card class="mx-4 members-card" elevation="0" flat>
+      <v-card-title>
+        <h5 class="group-role">Rol en el grupo: ADMINISTRADOR</h5>
+      </v-card-title>
 
-  <style>
+      <v-data-table
+      :items-per-page="itemsPerPage"
+      :headers="headers"
+      :items="members"
+      item-value="name"
+      class="elevation-1 members table">
+      </v-data-table>
+    </v-card>
+  </v-card>
+</template>
 
+<script>
+export default {
+  data() {
+    return {
+        itemsPerPage: 5,
+        headers: [
+          {
+            title: 'Miembros del grupo',
+            align: 'start',
+            sortable: false,
+            key: 'name',
+          },
+          { title: 'Rol en el grupo', align: 'end', key: 'rol' },
+          { title: 'Calificación en el grupo', align: 'end', key: 'calificación' },
+        ],
+        members: [
+          {
+            name: 'Marcos Hidalgo',
+            rol: 'Administrador',
+            calificación: 3.00,
+          },
+          {
+            name: 'Jonathan Figueroa',
+            rol: 'Moderador',
+            calificación: 3.50,
+          },
+          {
+            name: 'Ever Burga',
+            rol: 'Supervisor',
+            calificación: 4.00,
+          },
+          {
+            name: 'Gabriel Farfan',
+            rol: 'Miembro',
+            calificación: 4.50,
+          },
+          {
+            name: 'Sebas Salirrosas',
+            rol: 'Miembro',
+            calificación: 4.00,
+          },
+        ],
+      }
+  },
+  methods: {
+    navigateTo(route) {
+      // Implementa aquí la lógica para navegar a la vista correspondiente según la ruta proporcionada
+      console.log("Navegar a la vista:", route);
+    },
+    logout() {
+      // Implementa aquí la lógica para cerrar sesión
+      console.log("Cerrar sesión");
+    }
+  }
+};
+</script>
+
+<style scoped>
 .group-name {
   font-size: 24px;
   color: white;
@@ -26,5 +94,25 @@
   padding: 10px;
   margin-bottom: 16px;
 }
+.user-profile {
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+}
 
+.user-profile v-icon {
+  margin-right: 8px;
+}
+.user-profile .username {
+  font-size: 16px;
+  color: #616161;
+} 
+.members-card {
+  max-width: 50%;
+  margin: 0 auto;
+}
+
+.members-table {
+  width: 100%;
+}
 </style>
