70cd7afdd86215702bd999ee4780aa3e585983e6
GabrielF2311 <82108023+GabrielF2311@users.noreply.github.com>
2023-06-25 00:47:13 -0500

    Update GroupView.vue

--- src/views/public/grupos/GroupView.vue
+++ src/views/public/grupos/GroupView.vue
@@ -5,68 +5,68 @@
       <h2 class="group-name">Trabajo de DBD</h2>
     </v-card-title>
 
-    <v-card class="mx-4" elevation="0" flat>
-      <v-card-title>
-        <h5 class="group-role">Rol en el grupo: ADMINISTRADOR</h5>
-      </v-card-title>
+  <v-card class="mx-4" elevation="0" flat>
+    <v-card-title>
+      <h5 class="group-role">Rol en el grupo: ADMINISTRADOR</h5>
+    </v-card-title>
 
-      <v-card-title>
-        <h4 class="group-description"><b>Descripción del grupo:</b></h4>
-      </v-card-title>
+    <v-card-title>
+      <h4 class="group-description"><b>Descripción del grupo:</b></h4>
+    </v-card-title>
 
-      <v-card-text>
-        <v-container fluid>
-          <v-row justify="start">
-            <v-col cols="12" sm="6">
-              <v-card class="group-description-box">
-                <p class="group-description">
-                  Grupo formado por estudiantes que están llevando el curso de Diseño de Base de Datos, para la elaboración de un prototipo en base a la gestión de horarios.
-                </p>
-              </v-card>
-            </v-col>
+    <v-card-text>
+      <v-container fluid>
+        <v-row justify="start">
+          <v-col cols="12" sm="6">
+            <v-card class="group-description-box">
+              <p class="group-description">
+                Grupo formado por estudiantes que están llevando el curso de Diseño de Base de Datos, para la elaboración de un prototipo en base a la gestión de horarios.
+              </p>
+            </v-card>
+          </v-col>
 
-            <v-col cols="12" sm="6">
-              <v-card class="options-menu">
-                <v-list dense>
-                  <v-list-item class="option-item" v-for="(item, index) in optionsMenu" :key="index" @click="navigateTo(item.route)">
-                    <v-list-item-icon>
-                      <v-icon :color="item.color">{{ item.icon }}</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                      <v-list-item-title>{{ item.label }}</v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-item>
-                </v-list>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-    </v-card>
-
-    <v-card-actions class="options-actions">
-      <v-row align="center" justify="center">
-        <v-col cols="auto">
-          <v-btn color="primary" class="action-button" @click="navigateTo('members')">
-            <v-icon left>mdi-account-group</v-icon>
-            Miembros de grupo
-          </v-btn>          
-        </v-col>
-        <v-col cols="auto">
-          <v-btn color="primary" class="action-button" @click="navigateTo('events')">
-            <v-icon left>mdi-calendar</v-icon>
-            Eventos de grupo
-          </v-btn>
-        </v-col>
-        <v-col cols="auto">
-          <v-btn color="primary" class="action-button" @click="navigateTo('tasks')">
-            <v-icon left>mdi-format-list-bulleted</v-icon>
-            Tareas del grupo
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-card-actions>
+          <v-col cols="12" sm="6">
+            <v-card class="options-menu">
+              <v-list dense>
+                <v-list-item class="option-item" v-for="(item, index) in optionsMenu" :key="index" @click="navigateTo(item.route)">
+                  <v-list-item-icon>
+                    <v-icon :color="item.color">{{ item.icon }}</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>{{ item.label }}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
   </v-card>
+
+  <v-card-actions class="options-actions">
+    <v-row align="center" justify="center">
+      <v-col cols="auto">
+        <v-btn color="primary" class="action-button" @click="navigateTo('members')">
+          <v-icon left>mdi-account-group</v-icon>
+          Miembros de grupo
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn color="primary" class="action-button" @click="navigateTo('events')">
+          <v-icon left>mdi-calendar</v-icon>
+          Eventos de grupo
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn color="primary" class="action-button" @click="navigateTo('tasks')">
+          <v-icon left>mdi-format-list-bulleted</v-icon>
+          Tareas del grupo
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-card-actions>
+</v-card>
 </template>
 
 <script>
@@ -104,16 +104,18 @@ body {
 
 .group-role {
   margin-bottom: 16px;
+  font-size: 18px;
+  color: #616161;
 }
 
 .group-description {
-  font-size: 20px;
+  font-size: 18px;
   margin-bottom: 16px;
+  color: #424242;
 }
 
-
 .group-description-box {
-  background-color: #e3f2fd;
+  background-color: #f5f5f5;
   padding: 16px;
 }
 
@@ -122,6 +124,7 @@ body {
   display: inline-block;
   background-color: #e8eaf6;
   padding: 8px;
+  border-radius: 4px;
 }
 
 .option-item {
@@ -155,5 +158,40 @@ body {
 
 .options-actions {
   margin-top: 16px;
+}
+
+/* Estilos adicionales */
+@media (max-width: 600px) {
+  .group-description-box {
+    padding: 8px;
+  }
+  
+  .options-menu {
+    padding: 4px;
+  }
+  
+  .action-button {
+    font-size: 14px;
+    padding: 8px 12px;
+    margin: 0 8px;
+  }
+
+  .group-name {
+    display: flex;
+    align-items: center;
+  }
+
+  .group-name v-icon {
+    margin-right: 8px;
+  }
+
+  .options-actions {
+    margin-top: 32px;
+  }
+
+  .options-actions v-btn {
+    font-size: 16px;
+    padding: 12px 24px;
+  }
 }
 </style>
