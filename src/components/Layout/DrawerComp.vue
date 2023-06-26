e4bdc40631b6bfb41700017b651b6d8bc8c46dfd
4xeverburga <everburga@gmail.com>
2023-06-26 07:46:10 -0500

    fixing home

--- src/components/Layout/DrawerComp.vue
+++ src/components/Layout/DrawerComp.vue
@@ -5,16 +5,6 @@
 
 		<!-- button hamburguer -->
 		<v-app-bar-nav-icon class="hamburger" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-
-		<!-- logo -->
-		<v-card class="logo" elevation="0" flat color="transparent">
-			<v-img
-			:src="ccat_logo"
-			height="48"
-			width="48"
-			></v-img>
-		</v-card>
-
 		<v-app-bar-title class="ccat-name">unitime</v-app-bar-title>
 
 
@@ -55,21 +45,12 @@
 	left>
 		<v-list-item>
 			<v-list-item-content>
-				<v-list-item-title class="text-h6"> CCAT</v-list-item-title>
-				<v-list-item-subtitle> ...más que un centro</v-list-item-subtitle>
+				<v-list-item-title class="text-h6"> KEKEROS</v-list-item-title>
+				<v-list-item-subtitle> ... Bienvenido!</v-list-item-subtitle>
 			</v-list-item-content>
 		</v-list-item>
 		<v-divider></v-divider>
-		<v-list dense nav>
-			<v-list-item v-for="button in buttons" :key="button.id" link>
-				<v-list-item-icon>
-					<v-icon>{{ button.icon }}</v-icon>
-				</v-list-item-icon>
-				<v-list-item-content>
-					<v-list-item-title>{{ button.title }}</v-list-item-title>
-				</v-list-item-content>
-			</v-list-item>
-		</v-list>
+
 	</v-navigation-drawer>
 
 </template>
@@ -91,10 +72,7 @@ const buttons = ref(
 )
 
 const items = ref([
-	{id: 1, title: 'IDI'},
-	{id: 2, title: 'Academica'},
-	{id: 3, title: 'GTH'},
-	{id: 4, title: 'Logistica'},
+
 ])
 
 </script>
@@ -148,16 +126,10 @@ const items = ref([
 	margin-left: 40px;
 }
 
-/* when reaching mobile dimensions hide .areas-buttons and show the hamburger menu */
-@media (max-width: 600px) {
-	.areas-buttons{
-		display: none;
-	}
-	.hamburger{
-		display: block;
-	}
-	.logo{
-		margin-left: 0px;
-	}
+.hamburger{
+	display: block;
+}
+.logo{
+	margin-left: 0px;
 }
 </style>
--- src/views/public/home/HomeView.vue
+++ src/views/public/home/HomeView.vue
@@ -19,10 +19,7 @@
 		<v-card-text
 			class="ml-7"
 		>
-		El Centro Cultural Avanzada Tecnológica (CCAT) es una organización de estudiantes y egresados fundada en la
-		Facultad de Ingeniería Industrial y de Sistemas de la Universidad Nacional de Ingeniería, el 22 de mayo de 1991,
-		como una iniciativa estudiantil cuya motivación especial es emprender el reto de complementar la formación empresarial,
-		académica, cultural y humana de los futuros profesionales para lograr con ellos la construcción de una mejor sociedad.
+		Kekeros es una organización de estudiantes nacida del código 21-1 y con alcance a los estudiantes de ingeniería de sistemas del mismo...
 		</v-card-text>
 	</v-card>
 
@@ -52,8 +49,8 @@
 			</v-card-title>
 
 			<v-card-text>
-				Ser reconocido a nivel nacional como el principal centro universitario en formación empresarial, profesional
-				y de investigación.
+
+				
 			</v-card-text>
 
 		</v-card>
@@ -79,31 +76,20 @@
 			</v-card-title>
 
 			<v-card-text>
-				Contribuir con la formación humana de profesionales y líderes empresariales con vocación de servicio y actitud
-				de trabajo en equipo; profesionales competentes y capacitados que puedan emprender y enfrentar los retos que
-				nuestra sociedad requiere para su desarrollo.
+
+				
 			</v-card-text>
 
 		</v-card>
 	</v-col>
 </v-row>
-
-<!-- areas y aliados -->
-
-<br>
-<br>
-
-
-<AreasComp></AreasComp>
-
-<AliadosComp></AliadosComp>
 <!-- FIN -->
 
 </template>
 
 <script setup>
-import AreasComp from '@/components/Home/AreasComp.vue';
-import AliadosComp from '@/components/Home/AliadosComp.vue';
+
+
 </script>
 
 <style scoped>
