04899c19e2f7531cf44a0510273333dd5d777f3f
jonathanfigueroa28 <jonathan.figueroa.h@uni.pe>
2023-06-29 08:33:59 -0500

    b

--- src/views/public/tareas/TareasView.vue
+++ src/views/public/tareas/TareasView.vue
@@ -339,7 +339,156 @@
 </v-data-table>
 </v-container>
 
+<v-container class="bottom_table">
+	<v-data-table
+	:headers="headers_bottom"
+	:items="tareas"
+	:sort-by="[{ key: 'fecha', order: 'asc' }]"
+	class="elevation-1"
+	>
+	  <template v-slot:top>
+		<v-toolbar
+		flat
+		>
+		  <v-toolbar-title>Tareas del user en la etapa A del proyecto 1</v-toolbar-title>
+		  <v-divider
+			class="mx-4"
+			inset
+			vertical
+			></v-divider>
+			<v-spacer></v-spacer>
+			<v-dialog
+			v-model="dialogBottom"
+			max-width="500px"
+			>
+			<template v-slot:activator="{ props }">
+				<v-btn
+				color="primary"
+				dark
+				class="mb-2"
+				v-bind="props"
+				>
+				New Item
+			</v-btn>
+		</template>
+			<v-card>
+				<v-card-title>
+					<span class="text-h5">{{ formTitleBottom }}</span>
+				</v-card-title>
+  
+				<v-card-text>
+					<v-container>
+						<v-row>
+							<v-col
+							cols="12"
+							sm="6"
+							md="4"
+							>
+							<v-text-field
+						v-model="editedItemBottom.nombre"
+						label="Nombre de tarea"
+					  ></v-text-field>
+					</v-col>
+					<v-col
+					  cols="12"
+					  sm="6"
+					  md="4"
+					  >
+					  <v-text-field
+					  v-model="editedItemBottom.fecha"
+					  type="date"
+					  label="Fecha del tarea"
+					  ></v-text-field>
+					</v-col>
+					<v-col
+					  cols="12"
+					  sm="6"
+					  md="4"
+					  >
+					  <!-- time field -->
+					  <v-text-field
+					  v-model="editedItemBottom.hora"
+					  type="time"
+					  label="Hora de tarea"
+					  ></v-text-field>
+					</v-col>
+				</v-row>
+			</v-container>
+			  </v-card-text>
+  
+			  <v-card-actions>
+				<v-spacer></v-spacer>
+				<v-btn
+				  color="blue-darken-1"
+				  variant="text"
+				  @click="closeBottom"
+				  >
+				  Cancel
+				</v-btn>
+				<v-btn
+				color="blue-darken-1"
+				  variant="text"
+				  @click="saveBottom"
+				  >
+				  Save
+				</v-btn>
+			</v-card-actions>
+		</v-card>
+		  </v-dialog>
+		  <v-dialog v-model="dialogDeleteBottom" max-width="500px">
+			<v-card>
+				<v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+			  <v-card-actions>
+				  <v-spacer></v-spacer>
+				  <v-btn color="blue-darken-1" variant="text" @click="closeDeleteBottom">Cancel</v-btn>
+				  <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirmBottom">OK</v-btn>
+				<v-spacer></v-spacer>
+			  </v-card-actions>
+			</v-card>
+		</v-dialog>
+		</v-toolbar>
+	  </template>
+	  
+	<template v-slot:item.actions="{ item }">
+		<v-icon
+		size="small"
+		class="me-2"
+		  @click="editItemBottom(item.raw)"
+		  >
+		  mdi-pencil
+		</v-icon>
+		<v-icon
+		size="small"
+		@click="deleteItemBottom(item.raw)"
+		>
+		mdi-delete
+		</v-icon>
+		
+		<!-- select icon -->
+		<v-icon
+		size="small"
+		@click="deleteItemBottom(item.raw)"
+		>
+		mdi-checkbox-marked-circle
+		</v-icon>
+	</template>
+	<template v-slot:no-data>
+		<v-btn
+		color="primary"
+		@click="initialize"
+		>
+		Reset
+	</v-btn>
+	</template>
+</v-data-table>
+</v-container>
+
+
+
 </template>
+
+
+
 
 
 
@@ -661,7 +810,27 @@ import axios from 'axios'
 		getTareas(){
 
 		},
+		//JONATHANNNNNN
 
+		//Todas las tareas de un User
+		getTareasUser(){
+			//const selectedIndex = this.getIndexSelected();
+			//console.log("proyectos_all: ", this.proyectos_all)
+			//const idProyecto = this.proyectos_all[selectedIndex].id_proyecto;
+			//console.log("get request sent to ");
+			//console.log(this.pathToServer + '/etapas/'+idProyecto)
+			axios.get(this.pathToServer + '/tareasApi/etapas/'+idProyecto)
+			.then(response => {
+				// this.proyectos_all = response.data
+				console.log('response of getEtapas',response.data)
+				// assign result to proyectos_all 
+				this.etapas = response.data;
+				
+			}).catch(error => {
+				console.log(error)
+			})
+		},
+		//JONATHANNNNNN
 		// update button
 		handleSelection() {
 			this.getEtapas();
