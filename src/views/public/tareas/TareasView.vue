b6dc5a04faea57c3d819041350b3274724d4407c
jonathanfigueroa28 <jonathan.figueroa.h@uni.pe>
2023-06-29 14:15:41 -0500

    FINALIZADO TARAEAS DE ETAPA

--- src/views/public/tareas/TareasView.vue
+++ src/views/public/tareas/TareasView.vue
@@ -205,7 +205,7 @@
 		<v-toolbar
 		flat
 		>
-		  <v-toolbar-title>Tareas de etapa</v-toolbar-title>
+		  <v-toolbar-title>Tareas de etapa (A)</v-toolbar-title>
 		  <v-divider
 			class="mx-4"
 			inset
@@ -337,152 +337,6 @@
 	</template>
 </v-data-table>
 </v-container>
-
-<v-container class="bottom_table">
-	<v-data-table
-	:headers="headers_bottom"
-	:items="tareas"
-	:sort-by="[{ key: 'fecha', order: 'asc' }]"
-	class="elevation-1"
-	>
-	  <template v-slot:top>
-		<v-toolbar
-		flat
-		>
-		  <v-toolbar-title>Tareas del user en la etapa A del proyecto 1</v-toolbar-title>
-		  <v-divider
-			class="mx-4"
-			inset
-			vertical
-			></v-divider>
-			<v-spacer></v-spacer>
-			<v-dialog
-			v-model="dialogBottom"
-			max-width="500px"
-			>
-			<template v-slot:activator="{ props }">
-				<v-btn
-				color="primary"
-				dark
-				class="mb-2"
-				v-bind="props"
-				>
-				New Item
-			</v-btn>
-		</template>
-			<v-card>
-				<v-card-title>
-					<span class="text-h5">{{ formTitleBottom }}</span>
-				</v-card-title>
-  
-				<v-card-text>
-					<v-container>
-						<v-row>
-							<v-col
-							cols="12"
-							sm="6"
-							md="4"
-							>
-							<v-text-field
-						v-model="editedItemBottom.nombre"
-						label="Nombre de tarea"
-					  ></v-text-field>
-					</v-col>
-					<v-col
-					  cols="12"
-					  sm="6"
-					  md="4"
-					  >
-					  <v-text-field
-					  v-model="editedItemBottom.fecha"
-					  type="date"
-					  label="Fecha del tarea"
-					  ></v-text-field>
-					</v-col>
-					<v-col
-					  cols="12"
-					  sm="6"
-					  md="4"
-					  >
-					  <!-- time field -->
-					  <v-text-field
-					  v-model="editedItemBottom.hora"
-					  type="time"
-					  label="Hora de tarea"
-					  ></v-text-field>
-					</v-col>
-				</v-row>
-			</v-container>
-			  </v-card-text>
-  
-			  <v-card-actions>
-				<v-spacer></v-spacer>
-				<v-btn
-				  color="blue-darken-1"
-				  variant="text"
-				  @click="closeBottom"
-				  >
-				  Cancel
-				</v-btn>
-				<v-btn
-				color="blue-darken-1"
-				  variant="text"
-				  @click="saveBottom"
-				  >
-				  Save
-				</v-btn>
-			</v-card-actions>
-		</v-card>
-		  </v-dialog>
-		  <v-dialog v-model="dialogDeleteBottom" max-width="500px">
-			<v-card>
-				<v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
-			  <v-card-actions>
-				  <v-spacer></v-spacer>
-				  <v-btn color="blue-darken-1" variant="text" @click="closeDeleteBottom">Cancel</v-btn>
-				  <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirmBottom">OK</v-btn>
-				<v-spacer></v-spacer>
-			  </v-card-actions>
-			</v-card>
-		</v-dialog>
-		</v-toolbar>
-	  </template>
-	  
-	<template v-slot:item.actions="{ item }">
-		<v-icon
-		size="small"
-		class="me-2"
-		  @click="editItemBottom(item.raw)"
-		  >
-		  mdi-pencil
-		</v-icon>
-		<v-icon
-		size="small"
-		@click="deleteItemBottom(item.raw)"
-		>
-		mdi-delete
-		</v-icon>
-		
-		<!-- select icon -->
-		<v-icon
-		size="small"
-		@click="deleteItemBottom(item.raw)"
-		>
-		mdi-checkbox-marked-circle
-		</v-icon>
-	</template>
-	<template v-slot:no-data>
-		<v-btn
-		color="primary"
-		@click="initialize"
-		>
-		Reset
-	</v-btn>
-	</template>
-</v-data-table>
-</v-container>
-
-
 
 </template>
 
@@ -529,9 +383,10 @@ import axios from 'axios'
 				title:'Fecha',
 				key:'fecha',
 			},
+			
 			{
-				title:'Hora',
-				key:'hora',
+				title:'Nombre',
+				key:'nombre',
 			},
 			{
 				title:'Estado',
@@ -579,12 +434,14 @@ import axios from 'axios'
 		editedItemBottom: {
 			fecha:'',
 			hora:'',
+			nombre:'',
 			responsable:'',
 			estado:'',
 		},
 		defaultItemBottom: {
 			fecha:'',
 			hora:'',
+			nombre:'',
 			responsable:'',
 			estado:'',
 		},
@@ -630,52 +487,16 @@ import axios from 'axios'
 		this.tareas = [
 			{
 				fecha:'10-10-20',
-				hora:'10:00',
-				responsable:'Juan',
-				estado:'Pendiente',
+				nombre:'tarea1',
+				responsable:'pedro',
+				estado:'ausente',
 			},
 			{
 				fecha:'10-10-20',
-				hora:'10:00',
+				nombre:'tarea2',
 				responsable:'Juan',
 				estado:'Pendiente',
-			},
-			{
-				fecha:'10-10-20',
-				hora:'10:00',
-				responsable:'Juan',
-				estado:'Pendiente',
-			},
-			{
-				fecha:'10-10-20',
-				hora:'10:00',
-				responsable:'Juan',
-				estado:'Pendiente',
-			},
-			{
-				fecha:'10-10-20',
-				hora:'10:00',
-				responsable:'Juan',
-				estado:'Pendiente',
-			},
-			{
-				fecha:'10-10-20',
-				hora:'10:00',
-				responsable:'Juan',
-				estado:'Pendiente',
-			},
-			{
-				fecha:'10-10-20',
-				hora:'10:00',
-				responsable:'Juan',
-				estado:'Pendiente',
-			},
-			{
-				fecha:'10-10-20',
-				hora:'10:00',
-				responsable:'Juan',
-				estado:'Pendiente',
-			},
+			}
 		]
 		},
   
@@ -806,30 +627,26 @@ import axios from 'axios'
 				console.log(error)
 			})
 		},
-		
-		getTareas(){
-
-		},
 		//JONATHANNNNNN
-
-		//Todas las tareas de un User
-		getTareasUser(){
-			//const selectedIndex = this.getIndexSelected();
-			//console.log("proyectos_all: ", this.proyectos_all)
-			//const idProyecto = this.proyectos_all[selectedIndex].id_proyecto;
-			//console.log("get request sent to ");
-			//console.log(this.pathToServer + '/etapas/'+idProyecto)
-			axios.get(this.pathToServer + '/tareasApi/etapas/'+idProyecto)
+		//todas las tareas de la etapa ET123456
+		getTareas(){
+			const selectedIndex = this.getIndexSelected();
+			const idEtapa = this.etapas_all_fields[selectedIndex];
+			console.log("get request sent to ", this.pathToServer + '/tareas/' + 'ET123456');
+			axios.get(this.pathToServer + '/tareasApi/tareas/' + 'ET123456')
 			.then(response => {
 				// this.proyectos_all = response.data
-				console.log('response of getEtapas',response.data)
-				// assign result to proyectos_all 
-				this.etapas = response.data;
+				console.log('response of getTareas',response.data)
+				// assign result to proyectos_all
+				this.tareas = response.data
 				
-			}).catch(error => {
+			})
+			.catch(error => {
 				console.log(error)
 			})
 		},
+		
+
 		//JONATHANNNNNN
 		// update button
 		handleSelection() {
